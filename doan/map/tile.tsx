a3df92cb2c60be9680f8d8dcb924222f7dad36dd
lolisenpai21 <nguyenhaidang1172005@gmail.com>
2025-04-01 20:35:33 +0700

    Delete doan/map/tile.tsx

--- doan/map/tile.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tile" tilewidth="48" tileheight="48" tilecount="16" columns="4">
- <image source="../../download/tile.png" width="200" height="200"/>
-</tileset>
