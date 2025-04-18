a1adcc7c1dfedcf7588e61203d0909ec918e0a9e
lolisenpai21 <nguyenhaidang1172005@gmail.com>
2025-04-19 06:23:56 +0700

    Delete code/test.py

--- code/test.py
+++
@@ -1,9 +0,0 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-print([i for i in range(0,101) if i%2==0])
